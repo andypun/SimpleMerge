b8ff5b28ccbcc9e3b5e8e7fb355c9b66124548d1
andypun <apun6146@bths.edu>
2019-02-08 21:12:16 -0500

    Final

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -5,17 +5,31 @@
  * array of integers.
  */
 public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2)
-    {
-        int[] temp = new int[arr1.length + arr2.length];
-        int x=0, y=0, z=0;
+    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+        int[] merged = new int[arr1.length + arr2.length];
+        int x = 0;
+        int y=0;
+        int z=0;
+        while (x < merged.length) {
+            if (y == arr1.length || z == arr2.length) {
+                if (y == arr1.length) {
+                    merged[x++] = arr2[z++];
 
+                }
+                else {
+                    merged[x++] = arr1[y++];
+                }
+            }
+            else {
+                if (arr1[y] < arr2[z]) {
+                    merged[x++] = arr1[y++];
+                }
+                else {
+                    merged[x++] = arr2[z++];
+                }
+            }
+        }
 
-
-
-
-
-
+        return merged;
     }
-
 }
